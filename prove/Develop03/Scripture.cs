92a9434ad2d2bfc29a088137caf63c89e6e77dba
Toginomi <rudyiipilande@gmail.com>
2024-10-04 00:09:27 +0800

    stubs

---
+++ prove/Develop03/Scripture.cs
@@ -0,0 +1,33 @@
+public class Scripture
+{
+    private Reference _reference;
+    private List<Word> _words = new List<Word>();
+
+    public Scripture(Reference reference, string text)
+    {
+        _reference = reference;
+        _words = new List<Word>();
+
+        string[] words = text.Split(' ');
+        foreach (string word in words)
+        {
+            _words.Add(new Word(word));
+        }
+    }
+
+    public void HideRandomWords(int numberToHide)
+    {
+
+    }
+
+    public string GetDisplayText()
+    {
+        return "";
+    }
+
+    public bool IsCompletelyHidden()
+    {
+        return false;
+    }
+
+}
